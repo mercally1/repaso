ae9f0bff43ee866b0d30d0521039ef5dd7b3ce6a
mercally1 <31991854+mercally1@users.noreply.github.com>
2024-01-17 08:21:57 -0600

    Add files via upload

---
+++ Repaso.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repaso
+{
+    class Repaso
+    {
+        static void Main(string[] args)
+        {
+            /*
+            //variables
+            int x;
+            int y;
+
+            x = 10;
+            y = x + 5;
+            Console.WriteLine( y);
+            Console.ReadLine();
+            
+
+            Console.WriteLine("What is your name ");
+            Console.WriteLine("Type your first name ");
+            string myFirstName;
+            myFirstName = Console.ReadLine();
+
+            //string myLastName;
+            //Console.WriteLine("Type your last name");
+            //myLastName = Console.ReadLine();
+
+            Console.WriteLine("Type your last name ");
+            string myLastName = Console.ReadLine();
+
+            Console.WriteLine("Hello " + myFirstName + " " + myLastName);
+            //Console.WriteLine($"Hello, {myFirstName } {myLastName}");
+            Console.ReadLine();
+            */
+
+            /*
+            Console.WriteLine("bob´s Big Giveaway ");
+            Console.WriteLine("Choose the door: 1, 2, 3");
+            string userValue = Console.ReadLine();
+
+            string message;
+            
+            if (userValue == "1")
+            {
+                string message = " you won a new car";
+                Console.WriteLine(message);
+            }
+            else if (userValue == "2")
+            {
+                string message = "you won a boot";
+                Console.WriteLine(message);
+            }
+            else if (userValue == "3")
+            {
+                string message = "you won the cat";
+                Console.WriteLine(message);
+            }
+
+            else
+            {
+                string message = " sorry , no won ";
+                Console.WriteLine(message);
+            }
+            
+
+            if (userValue == "1")
+                message = " you won a new car";
+            else if (userValue == "2")
+                message= "you won a boot";
+            else if (userValue == "3")
+                message = "you won the cat";
+            else
+                message = " sorry , no won ";
+                
+            Console.WriteLine(message);
+            Console.ReadLine();
+            */
+            /*  for (int i = 0; i < 10; i++)
+               {
+                   Console.WriteLine(i);
+               }
+               Console.ReadLine();
+            */
+            /*
+               int b = 0;
+
+               while (b < 10)
+               {
+
+                   Console.WriteLine(b);
+                   b++;  
+               }
+               Console.ReadLine();
+            */
+
+            //int[] number = new int[5] { 2, 6, 4, 5, 4 };
+            /*
+            number[0] = 1;
+            number[1] = 23;
+            number[2] = 3;
+            number[3] = 16;
+            number[4] = 17;
+            *
+            Console.WriteLine("Type a number");
+            while (number.Length < 4)
+            {
+                
+                Console.WriteLine(number[3]);||||||
+           
+            }
+
+           // Console.WriteLine(number[1]);
+            Console.ReadLine();
+            */
+
+
+            //string myString = "my  \"so - called \" life";
+            //string myString = "What if I need  a \n new line";
+            //string myString = "Go to your c:\\ drive";
+            //string myString = @"Go to your c:\ drive ";
+            //string myString = "That summer we took threes across the board";
+
+            //myString = myString.Substring(6, 20);
+            //myString = myString.ToUpper();
+            //myString = myString.Replace(" ", " -- ");
+            //myString = myString.Remove(1, 4);
+            /*
+            StringBuilder myString = new StringBuilder();
+
+            for (int i = 0; i < 100; i++)
+            {
+                myString .Append(" - ");
+                myString.Append(i);
+            }
+            Console.WriteLine(myString);
+            Console.ReadLine();
+            */
+
+            // DateTime myValue = DateTime.Now;
+            //Console.WriteLine(myValue.ToString()); //date and time complete
+            // Console.WriteLine(myValue.ToShortDateString());//short date
+            // Console.WriteLine(myValue.ToShortTimeString());// short time
+            //Console.WriteLine(myValue.ToLongTimeString());// long time
+            //Console.WriteLine(myValue.ToLongDateString());// long date
+
+            //Console.WriteLine(myValue.AddDays(5).ToLongDateString());
+            // Console.WriteLine(myValue.AddHours(3).ToLongTimeString());
+            // Console.WriteLine(myValue.AddHours(-3).ToLongTimeString());
+
+            //Console.WriteLine(myValue.Month);
+
+            
+            //Console.ReadLine();
+        }
+
+
+    }
+}
